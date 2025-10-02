d96b391e74f6877278bb261d070bc78823d9e4b8
joplayxyz <joplayyt@web.de>
2025-10-02 23:22:28 +0200

    Fix Colorgrid returning rgb codes instead of hexcodes

--- _assets/js/colorgrid.js
+++ _assets/js/colorgrid.js
@@ -1,13 +1,28 @@
 document.addEventListener('click', async (e) => {
   const el = e.target.closest('.colorbox');
   if (!el) return;
+
   let bg = el.style.backgroundColor || getComputedStyle(el).backgroundColor;
-  const text = `style="background-color: ${bg};"`;
+
+  // rgb/rgba to hex
+  function rgbToHex(rgb) {
+    const result = rgb.match(/\d+/g).map(Number).slice(0, 3); // r,g,b
+    return (
+      "#" +
+      result
+        .map((x) => x.toString(16).padStart(2, "0"))
+        .join("")
+        .toUpperCase()
+    );
+  }
+
+  let hex = bg.startsWith("rgb") ? rgbToHex(bg) : bg; // if its hexcode keep it that 
+
   try {
-    await navigator.clipboard.writeText(text);
-    showToast('Copied:', text);
+    await navigator.clipboard.writeText(hex);
+    showToast("Copied:", hex);
   } catch (err) {
-    showToast('Copy failed', err);
-    console.error('Copy failed', err);
+    showToast("Copy failed", err);
+    console.error("Copy failed", err);
   }
 });
